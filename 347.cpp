23dad5a085d70ea80d4a1e1ef17eb62932ffd6c0
PursuitCane <pursuit.cane@gmail.com>
2025-02-02 16:45:26 +0800

    25-2-2

--- 347.cpp
+++ 347.cpp
@@ -6,19 +6,29 @@ public:
             m[num]++;
         }
 
-        auto maxHeapCompare = [](pair<int, int> left, pair<int, int> right) {
-		return left.second < right.second; // 自定义比较器，建立最大堆
+        auto minHeapCompare = [](pair<int, int> left, pair<int, int> right) {
+		return left.second > right.second; // 自定义比较器，建立最小堆
 };
-std::priority_queue<pair<int, int>, std::vector<pair<int, int>>, decltype(maxHeapCompare)>
-            heap(maxHeapCompare);
+std::priority_queue<pair<int, int>, std::vector<pair<int, int>>, decltype(minHeapCompare)>
+            heap(minHeapCompare);
 
         for (auto item : m) {
-            pair<int, int> p = make_pair(item.first, item. second);
-            heap.push(p);
+            if (heap.size() < k) {
+                pair<int, int> p = make_pair(item.first, item. second);
+                heap.push(p);
+                continue;
+            }
+            if (item.second > heap.top().second) {
+                heap.pop();
+                pair<int, int> p = make_pair(item.first, item. second);
+                heap.push(p);
+            }
+
+            
         }
 
         vector<int> ans;
-        while (k--) {
+        while (heap.empty() == false) {
             ans.push_back(heap.top().first);
             heap.pop();
         }
