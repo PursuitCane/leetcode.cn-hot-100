2a8213bcbb8f1282018ccd3a792294170e6f5c70
PursuitCane <pursuit.cane@gmail.com>
2025-02-06 23:58:48 +0800

    25-2-6

---
+++ 279.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + 1;
+             for (int j = 2; j * j <= i; j++) {
+                dp[i] = min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+        return dp[n];
+        
+    }
+};
